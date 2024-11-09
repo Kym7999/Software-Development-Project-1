42dbe6db440e21aea19040570a8cf60a97b1a401
Glenn Tavas <glenn.tavas@gmail.com>
2024-11-09 15:01:41 -0500

    added nurse and health coach UI

---
+++ Health Coach/searchPatient.js
@@ -0,0 +1,7 @@
+// NOT YET IMPLEMENTED: search button functionality - the execution of SQL query
+// and display results in the "search-results" div
+
+function showResults() {
+    document.getElementById("results").style.display = "block";
+  }
+  
