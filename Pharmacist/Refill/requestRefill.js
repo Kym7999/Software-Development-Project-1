57973b0ca9a0e329438682908be414f02088d558
“Giang” <hdang23@my.centennialcollege.ca>
2024-11-26 10:48:23 -0500

    Update for the Iteration 2

---
+++ Pharmacist/Refill/requestRefill.js
@@ -0,0 +1,11 @@
+// Function to handle Approve action
+function ApproveRefill() {
+    // Show an alert confirming approval and notification message
+    alert("The medication refill request was approved—and a notification was sent to the patient.");
+}
+
+// Function to handle Deny action
+function DenyRefill() {
+    // Show an alert confirming denial and notification message
+    alert("The refill request has been denied-and a notification was sent to the patient");
+}
